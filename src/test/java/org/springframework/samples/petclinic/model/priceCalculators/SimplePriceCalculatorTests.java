67a7784555e23d04336c5b2b5511eb9c9f8d46d0
Slifer-The-Sky-Dragon <aghaei.alirezza@gmail.com>
2021-11-16 22:38:12 +0330

    improve test

--- src/test/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculatorTests.java
@@ -1,4 +1,4 @@
-package org.springframework.samples.petclinic.model;
+package org.springframework.samples.petclinic.model.priceCalculators;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
